819b66736fb5a11ac62e4155baf97070d1dd7f68
Raja Reddy <klnrajareddy@gmail.com>
2012-01-03 15:47:45 +0530

    Adding js files

---
+++ js/course_data.js
@@ -0,0 +1,165 @@
+var courseData = {
+            // Course Starts
+            "data":
+            {
+                "introduction" : "welcome.wav",
+                "menu" : "level_selection.wav"
+            },
+            // Levels
+            "children" :
+            [
+                // Level 1
+                {
+                    "data" :
+                    {
+                        "introduction" : "level_1_intro.wav",
+                        "menu" : "level_1_menu.wav",
+                    },
+                    //Chapters
+                    "children" :
+                    [
+                        //Chapter 1
+                        {
+                            "data" :
+                            {
+                                "introduction" : "level_1_chapter_1_intro.wav",
+                                "menu" : "level_1_chapter_1_menu.wav",
+                            },
+                            //Lessons
+                            "children" :
+                            [
+                                // Lesson 1
+                                {
+                                    "data" :
+                                    {
+                                        "lesson": "chapter_1_lesson_1.wav",
+                                    },
+                                    "children" :
+                                    [
+                                    ],
+                                },
+                                // Lesson 2
+                                {
+                                    "data" :
+                                    {
+                                        "lesson": "chapter_1_lesson_2.wav",
+                                    },
+                                    "children" :
+                                    [
+                                    ],
+                                }
+                            ]
+                        },
+                        // Chapter 2
+                        {
+                            "data" :
+                            {
+                                "introduction" : "level_1_chapter_2_intro.wav",
+                                "menu" : "level_1_chapter_2_menu.wav",
+                            },
+                            //Lessons
+                            "children" :
+                            [
+                                // Lesson 1
+                                {
+                                    "data" :
+                                    {
+                                        "lesson": "chapter_2_lesson_1.wav",
+                                    },
+                                    "children" :
+                                    [
+                                    ],
+                                },
+                                // Lesson 2
+                                {
+                                    "data" :
+                                    {
+                                        "lesson": "chapter_2_lesson_2.wav",
+                                    },
+                                    "children" :
+                                    [
+                                    ],
+                                }
+                            ]
+                        }
+                    ]
+                },
+                // Level 2
+                {
+                    "data" :
+                    {
+                        "introduction" : "level_2_intro.wav",
+                        "menu" : "level_2_menu.wav",
+                    },
+                    //Chapters
+                    "children" :
+                    [
+                        //Chapter 1
+                        {
+                            "data" :
+                            {
+                                "introduction" : "level_2_chapter_1_intro.wav",
+                                "menu" : "level_2_chapter_1_menu.wav",
+                            },
+                            //Lessons
+                            "children" :
+                            [
+                                // Lesson 1
+                                {
+                                    "data" :
+                                    {
+                                        "lesson": "chapter_1_lesson_1.wav",
+                                    },
+                                    "children" :
+                                    [
+                                    ],
+                                },
+                                // Lesson 2
+                                {
+                                    "data" :
+                                    {
+                                        "lesson": "chapter_1_lesson_2.wav",
+                                    },
+                                    "children" :
+                                    [
+                                    ],
+                                }
+                            ]
+                        },
+                        // Chapter 2
+                        {
+                            "data" :
+                            {
+                                "introduction" : "level_2_chapter_2_intro.wav",
+                                "menu" : "level_2_chapter_2_menu.wav",
+                            },
+                            //Lessons
+                            "children" :
+                            [
+                                // Lesson 1
+                                {
+                                    "data" :
+                                    {
+                                        "lesson": "chapter_2_lesson_1.wav",
+                                    },
+                                    "children" :
+                                    [
+                                    ],
+                                },
+                                // Lesson 2
+                                {
+                                    "data" :
+                                    {
+                                        "lesson": "chapter_2_lesson_2.wav",
+                                    },
+                                    "children" :
+                                    [
+                                    ],
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+            //Course Ends
+    }
---
+++ js/metadata.js
@@ -0,0 +1,1 @@
+var metaData = {"audioFileBase": "./audio/"};
