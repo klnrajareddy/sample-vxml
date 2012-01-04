51f860e12ec1b4bd74336f43de109251aa14103e
Raja Reddy <klnrajareddy@gmail.com>
2012-01-04 11:44:25 +0530

    Added more metadata for the invalid/no input WAVs

--- js/metadata.js
+++ js/metadata.js
@@ -1,1 +1,7 @@
-var metaData = {"audioFileBase": "./audio/"};
+var metaData = {
+	"audioFileBase": "./audio/",
+	"noInputRetryAudio": "error.no.input.retry.wav",
+	"noInputDisconnectAudio": "error.no.input.disconnect.wav",
+	"invalidInputRetryAudio": "error.wrong.input.retry.wav",
+	"invalidInputDisconnectAudio": "error.wrong.input.disconnect.wav",
+};
