a4710ae4fcf109a24d1e6ea85ca48e7fb59bb3d3
Aravind SV <arvind.sv@gmail.com>
2012-01-17 11:38:54 +0530

    Trying out certification course.

---
+++ certification_course/js/certification_call_context.js
@@ -0,0 +1,40 @@
+var CertificationCallContext = function(course, metadata) {
+    this.init = function(course, metadata) {
+        Course.buildLinks(course);
+        this.course = course;
+        this.currentInteraction = course.children[0].children[0];
+        this.metadata = metadata;
+    };
+
+    this.isAtCourseRoot = function() {
+        return false;
+    };
+
+    this.isAtLesson = function() {
+        return true;
+    };
+
+    this.currentInteractionLesson = function() {
+        return findContentByName("lesson");
+    };
+
+    this.currentInteractionMenu = function() {
+        return findContentByName("menu");
+    };
+
+    this.lessonFinished = function() {
+        this.currentInteraction = this.currentInteraction.siblingOnRight;
+    };
+
+    this.findContentByName = function(contentName) {
+        var contents = this.currentInteraction.contents
+        var contentLength = contents.length
+        for(i = 0; i< contentLength; i++){
+            if(contents[i].name == contentName)
+                return contents[i];
+        }
+        return undefined;
+    };
+
+    this.init(course, metadata);
+};
